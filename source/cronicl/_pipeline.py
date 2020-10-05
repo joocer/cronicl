01bea8e37748ff42d0e66e27483c70028e31c75e
Justin Joyce <justin.joyce@joocer.com>
2020-10-05 20:07:44 +0100

    pipeline validation routines

--- source/cronicl/_pipeline.py
+++ source/cronicl/_pipeline.py
@@ -1,4 +1,4 @@
-import time, logging
+import time, logging, warnings
 import networkx as nx
 from .stages import PassThruStage
 
@@ -6,20 +6,34 @@ from .stages import PassThruStage
 def always_pass(x):
     return True
 
-def empty(x):
-    return []
-
 
 class Pipeline(object):
 
     def __init__(self, graph):
         self.graph = graph
         self.all_stages = self.graph.nodes()
-        # validate the graph
-        # - can't be cyclic
-        # - can't have orphans
-        # - each node has a function attribute
-        # - the object on the function attribute as an execute method
+
+        # VALIDATE THE GRAPH
+        # must have exactly one pump
+        pump = [ node for node in self.all_stages if len(graph.in_edges(node)) == 0 ]
+        if len(pump) != 1:
+            raise Exception("Pipelines must have exactly one 'pump' (nodes with no incoming edges)")
+        self.pump = pump[0]
+
+        # The pipeline can't be cyclic
+        try:
+            nx.find_cycle(graph, orientation="original")
+        except:
+            pass
+
+        # Every stage node must have a function attribute
+        if not all([graph.nodes()[node].get('function') for node in self.all_stages]):
+            raise Exception("All stages in the Pipeline must have a 'function' attribute")
+
+        # Every object on the function attribute must have an execute method
+        if not all([hasattr(graph.nodes()[node]['function'], 'execute') for node in self.all_stages]):
+            raise Exception("The object on all 'function' attributes in a Pipeline must have an 'execute' method")
+
         logging.debug('loaded a pipeline with {} stages'.format(len(self.all_stages)))
 
     def execute(self, record, **kwargs):
@@ -31,14 +45,11 @@ class Pipeline(object):
             if hasattr(stage_function, 'init'):
                 stage_function.init(**kwargs)
 
-        # get all the nodes with no incoming edges
-        pumps = [ node for node in self.all_stages if len(self.graph.in_edges(node)) == 0 ]
-        for pump in pumps:
-            logging.debug('running pump \'{}\', for record: {}'.format(pump, record))
-            # run the state and force the expansion of the results to
-            # run the pipeline, the sinks should persist the results
-            # so we should be able to discard the final results.
-            [ always_pass(x) for x in (self._inner_execute(pump, record) or []) ]
+        logging.debug('running pump \'{}\', for record: {}'.format(self.pump, record))
+        # run the state and force the expansion of the results to
+        # run the pipeline, the sinks should persist the results
+        # so we should be able to discard the final results.
+        [ always_pass(x) for x in [ x for x in (self._inner_execute(self.pump, record) or []) ] ]
 
         # call all the closes
         for stage in self.all_stages:
@@ -74,7 +85,7 @@ class Pipeline(object):
                 # if the data 'passes' the filter, execute the next stage
                 if data_filter(result):
                     yield from self._inner_execute(next_stage, result)
-        return
+        yield
 
     # adapted from https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
     def tree(self, node, prefix=''):
@@ -95,10 +106,8 @@ class Pipeline(object):
                 yield from self.tree(child_node, prefix=prefix+extension)
 
     def draw(self):
-        pumps = [ node for node in self.graph.nodes() if len(self.graph.in_edges(node)) == 0 ]
-        for pump in pumps:
-            t = self.tree(pump)
-            print(pump)
-            print('\n'.join(t))
+        t = self.tree(self.pump)
+        print(self.pump)
+        print('\n'.join(t))
 
         
