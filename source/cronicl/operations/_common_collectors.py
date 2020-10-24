0611a2d4a9162d00f12f9adc4f73de3b8e9c467f
Justin Joyce <justin.joyce@joocer.com>
2020-10-24 23:16:02 +0100

    refactored collector

--- source/cronicl/operations/_common_collectors.py
+++ source/cronicl/operations/_common_collectors.py
@@ -7,17 +7,18 @@ A set of collectors for reuse
 from ._operation import Operation
 from .._signals import Signals
 from ._messages import create_new_message
+import abc
 
-#####################################################################
-
-class SumCollector(Operation):
-    """
-    Sums a set of fields in a message ignoring non-numeric values
-    """
+class BaseCollector(Operation):
     def __init__(self, fields):
         self.fields = fields
         self.accumulated = { k:0 for k in fields }
         Operation.__init__(self)
+
+    def read_sensor(self):
+        readings = Operation.read_sensor(self)
+        readings = { **readings, **self.accumulated }
+        return readings
 
     def execute(self, message):
         payload = message.payload
@@ -30,88 +31,40 @@ class SumCollector(Operation):
             self.accumulated = { k:0 for k in self.fields }
         else:
             for k in self.fields:
-                v = payload.get(k, 0)
-                try:
-                    self.accumulated[k] += float(v)
-                except ValueError:
-                    pass
+                v = payload.get(k, None)
+                if v:
+                    self.process(k, v)
 
         message.payload = payload
         return [message]
 
-    def read_sensor(self):
-        readings = Operation.read_sensor(self)
-        readings = { **readings, **self.accumulated }
-        return readings
+    @abc.abstractmethod
+    def process(field, value): pass
 
-class MaxCollector(Operation):
-    """
-    Returns max for each of a set of fields
-    """
-    def __init__(self, fields):
-        self.fields = fields
-        self.accumulated = { k:None for k in fields }
-        Operation.__init__(self)
+#####################################################################
 
-    def execute(self, message):
-        payload = message.payload
+class SumCollector(BaseCollector):
 
-        if payload == Signals.EMIT:
-            payload = self.accumulated
-            payload['source'] = self.operation_name
-            return [create_new_message(payload)]
-        elif payload == Signals.RESET:
-            self.accumulated = { k:None for k in self.fields }
-        else:
-            for k in self.fields:
-                v = payload.get(k)
-                if not v:
-                    pass
-                if not self.accumulated[k]:
-                    self.accumulated[k] = payload.get(k)
-                elif v > self.accumulated[k]:
-                    self.accumulated[k] = v
+    def process(self, field, value):
+        try:
+            self.accumulated[field] += float(value)
+        except ValueError:
+            pass
 
-        message.payload = payload
-        return [message]
 
-    def read_sensor(self):
-        readings = Operation.read_sensor(self)
-        readings = { **readings, **self.accumulated }
-        return readings
+class MaxCollector(BaseCollector):
 
-class MinCollector(Operation):
-    """
-    Returns max for each of a set of fields
-    """
-    def __init__(self, fields):
-        self.fields = fields
-        self.accumulated = { k:None for k in fields }
-        Operation.__init__(self)
+    def process(self, field, value):
+        if not self.accumulated[field]:
+            self.accumulated[field] = value
+        elif value > self.accumulated[field]:
+            self.accumulated[field] = value
 
-    def execute(self, message):
-        payload = message.payload
 
-        if payload == Signals.EMIT:
-            payload = self.accumulated
-            payload['source'] = self.operation_name
-            return [create_new_message(payload)]
-        elif payload == Signals.RESET:
-            self.accumulated = { k:None for k in self.fields }
-        else:
-            for k in self.fields:
-                v = payload.get(k)
-                if not v:
-                    pass
-                if not self.accumulated[k]:
-                    self.accumulated[k] = payload.get(k)
-                elif v < self.accumulated[k]:
-                    self.accumulated[k] = v
+class MinCollector(BaseCollector):
 
-        message.payload = payload
-        return [message]
-
-    def read_sensor(self):
-        readings = Operation.read_sensor(self)
-        readings = { **readings, **self.accumulated }
-        return readings
+    def process(self, field, value):
+        if not self.accumulated[field]:
+            self.accumulated[field] = value
+        elif value < self.accumulated[field]:
+            self.accumulated[field] = value
