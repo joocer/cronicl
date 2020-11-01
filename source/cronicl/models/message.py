990bfdf0000fb70e628191d09557c270b20c6f42
Justin Joyce <justin.joyce@joocer.com>
2020-11-01 21:36:35 +0000

    formatting

--- source/cronicl/models/message.py
+++ source/cronicl/models/message.py
@@ -3,7 +3,7 @@ Implements an envelope for payloads.
 
 The envelope includes meta information about the payload, for example
 a unique identifier and a flag for if the message and its
-descendants are being traced. 
+descendants are being traced.
 
 The message also carries a set of attributes, this allows key/value
 information separate to the payload to be sent with the payload.
@@ -19,47 +19,60 @@ from ..utils.serialization import dict_to_json
 
 
 class Message(object):
-
-    def __init__(self, payload=None, traced=False, topic='dead_letters', initializer=None):
+    def __init__(self, payload=None, traced=False, initializer=None):
         # unique identifier for this message
-        self.id = str(uuid.uuid4()) 
+        self.id = str(uuid.uuid4())
         self.payload = payload
         self.attributes = {}
         self.traced = traced
         self.initializer = initializer
         if not self.initializer:
             self.initializer = self.id
-        
 
     def __repr__(self):
         return self.payload
 
-
     def __str__(self):
         return str(self.payload)
 
-
-    def trace(self, operation='not defined', version='0'*8, child='', execution_start=0, execution_duration=0, force=False):
+    def trace(
+        self,
+        operation="not defined",
+        version="0" * 8,
+        child="",
+        execution_start=0,
+        execution_duration=0,
+        force=False,
+    ):
         if self.traced or force:
             sanitized_record = sanitize_record(self.payload, self.initializer)
             try:
                 sanitized_record = dict_to_json(sanitized_record)
             except ValueError:
                 sanitized_record = str(sanitized_record)
-            get_tracer().emit(self.id, execution_start, execution_duration, operation, version, child, self.initializer, sanitized_record)
+            get_tracer().emit(
+                self.id,
+                execution_start,
+                execution_duration,
+                operation,
+                version,
+                child,
+                self.initializer,
+                sanitized_record,
+            )
 
 
 def create_new_message(payload, sample_rate=0):
     """
-    Factory for messages, includes logic to select messages for 
+    Factory for messages, includes logic to select messages for
     random sampling.
     """
     if sample_rate >= 1:
         traced = True
     elif sample_rate > 0:
-        traced = random.randint(1, round(1/sample_rate)) == 1
+        traced = random.randint(1, round(1 / sample_rate)) == 1
     else:
         traced = False
     message = Message(payload=payload, traced=traced)
-    message.trace(operation='Create Message', child=message.id)
+    message.trace(operation="Create Message", child=message.id)
     return message
--- source/cronicl/models/pipeline.py
+++ source/cronicl/models/pipeline.py
@@ -11,8 +11,12 @@ from ..http import api_initializer
 
 
 class Pipeline(object):
-
-    def __init__(self, graph, sample_rate=0.001, enable_api=True, api_port=8000):
+    def __init__(
+            self,
+            graph,
+            sample_rate=0.001,
+            enable_api=True,
+            api_port=8000):
         self.threads = []
         self.paths = {}
         self.graph = graph
@@ -21,10 +25,12 @@ class Pipeline(object):
 
         # tracing can be resource heavy, so we trace a sample
         # default sampling rateis 0.1% (one per thousand)
-        self.sample_rate = sample_rate 
+        self.sample_rate = sample_rate
 
         # get the entry nodes - the ones with 0 incoming nodes
-        self.entry_nodes = [ node for node in self.all_operations if len(graph.in_edges(node)) == 0 ]
+        self.entry_nodes = [
+            node for node in self.all_operations if len(graph.in_edges(node)) == 0
+        ]
 
         # VALIDATE THE GRAPH
         # The pipeline can't be cyclic
@@ -34,67 +40,76 @@ class Pipeline(object):
         except nx.NetworkXNoCycle:
             has_loop = False
         if has_loop:
-            raise ValidationError("Pipeline must not be cyclic, if unsure do not have more than on incoming connection on any operations.")
-
+            raise ValidationError(
+                "Pipeline must not be cyclic, if unsure do not have more than on incoming connection on any operations."
+            )
         # Every operation node must have a function attribute
-        if not all([graph.nodes()[node].get('function') for node in self.all_operations]):
-            raise ValidationError("All Operations in the Pipeline must have a 'function' attribute.\nIf all Operations have a 'function', check Connector definitions for errors in Operation names.")
-
+        if not all(
+            [graph.nodes()[node].get("function") for node in self.all_operations]
+        ):
+            raise ValidationError(
+                "All Operations in the Pipeline must have a 'function' attribute.\nIf all Operations have a 'function', check Connector definitions for errors in Operation names."
+            )
         # Every object on the function attribute must have an execute method
-        if not all([hasattr(graph.nodes()[node]['function'], 'execute') for node in self.all_operations]):
-            raise ValidationError("The object on all 'function' attributes in a Pipeline must have an 'execute' method")
-
+        if not all(
+            [
+                hasattr(graph.nodes()[node]["function"], "execute")
+                for node in self.all_operations
+            ]
+        ):
+            raise ValidationError(
+                "The object on all 'function' attributes in a Pipeline must have an 'execute' method"
+            )
         if enable_api:
             # the very start of the HTTP Interface
-            api_thread = threading.Thread(target=api_initializer, args=(self,api_port))
+            api_thread = threading.Thread(target=api_initializer, args=(self, api_port))
             api_thread.daemon = True
             api_thread.start()
-
-        logging.debug('loaded a pipeline with {} operations, {} entry point(s)'.format(len(self.all_operations), len(self.entry_nodes)))
-
+        logging.debug(
+            "loaded a pipeline with {} operations, {} entry point(s)".format(
+                len(self.all_operations), len(self.entry_nodes)
+            )
+        )
 
     def reply_handler(self):
         """
-        Accept messages on the reply queue, replies attest the 
-        operation they have come from, we work out the next 
+        Accept messages on the reply queue, replies attest the
+        operation they have come from, we work out the next
         operations and put the message onto their queue.
 
         queue.get() is blocking so this should be run in a separate
         thread.
 
-        This is called for every message in the system, so for 
+        This is called for every message in the system, so for
         performance it caches key information. Although not a pure
         function, it is deterministic and idempotent, which should
         make it thread-safe.
         """
-        queue = get_queue('reply')
+        queue = get_queue("reply")
         response = queue.get()
         while not response == Signals.TERMINATE:
             respondent, message = response
-            # If it's the first time we've seen this respondent, 
+            # If it's the first time we've seen this respondent,
             # cache its path.
             if not self.paths.get(respondent):
                 self.paths[respondent] = []
                 outgoing_edges = self.graph.out_edges(respondent, default=[])
                 for this_operation, next_operation in outgoing_edges:
-                    data_filter = self.graph.get_edge_data(respondent, next_operation).get('filter', lambda x: True)
+                    data_filter = self.graph.get_edge_data(
+                        respondent, next_operation
+                    ).get("filter", lambda x: True)
                     self.paths[respondent].append((next_operation, data_filter))
-            
             for next_operation, data_filter in self.paths.get(respondent):
                 if message and data_filter(message):
                     get_queue(next_operation).put(message, False)
-
             response = queue.get()
-
         # None is used to terminate the handler
         logging.debug("REPLY handler got TERM signal")
 
-
     def init(self, **kwargs):
-
         def clamp(value, low_bound, high_bound):
             if value <= low_bound:
-                return low_bound 
+                return low_bound
             if value >= high_bound:
                 return high_bound
             return value
@@ -102,36 +117,43 @@ class Pipeline(object):
         # call all the operation inits, pass the kwargs
         for operation in self.all_operations:
             operation_node = self.graph.nodes()[operation]
-            operation_function = operation_node.get('function', NullOperation())
-            if hasattr(operation_function, 'init'):
+            operation_function = operation_node.get("function", NullOperation())
+            if hasattr(operation_function, "init"):
                 operation_function.init(**kwargs)
             # operations need to be told their name
-            operation_function.operation_name = operation 
-            operation_function.sample_rate = clamp(operation_node.get('sample_rate', 0), 0, 1)
-            operation_function.retry_count = clamp(operation_node.get('retry_count', 0), 0, 10)
-            operation_function.retry_delay = clamp(operation_node.get('retry_delay', 0), 0, 300)
+            operation_function.operation_name = operation
+            operation_function.sample_rate = clamp(
+                operation_node.get("sample_rate", 0), 0, 1
+            )
+            operation_function.retry_count = clamp(
+                operation_node.get("retry_count", 0), 0, 10
+            )
+            operation_function.retry_delay = clamp(
+                operation_node.get("retry_delay", 0), 0, 300
+            )
         self.initialized = True
 
         for operation in self.all_operations:
             # Operations can define a number of threads to create.
             #
-            # This is intended to be used by operations with IO, if 
-            # the operation is just working in memory, 
-            # multi-threading is not advised as locks are likely to 
-            # cause slow processing. It is important to remember, 
-            # Python does not concurrently run threads, one thread 
+            # This is intended to be used by operations with IO, if
+            # the operation is just working in memory,
+            # multi-threading is not advised as locks are likely to
+            # cause slow processing. It is important to remember,
+            # Python does not concurrently run threads, one thread
             # runs whilst the other wait.
-            thread_count = self.graph.nodes()[operation].get('concurrency', 1)
+            thread_count = self.graph.nodes()[operation].get("concurrency", 1)
             # clamp the number of threads between 1 and 5
             thread_count = clamp(thread_count, 1, 5)
-            
-            operation_function = self.graph.nodes()[operation].get('function', NullOperation())
+
+            operation_function = self.graph.nodes()[operation].get(
+                "function", NullOperation()
+            )
             for _ in range(thread_count):
-                thread=threading.Thread(target=operation_function.run)
+                thread = threading.Thread(target=operation_function.run)
                 thread.daemon = True
                 thread.start()
                 self.threads.append(thread)
-
         # Create multiple threads to handle replies
         # The reply_handler has no locks and testing has shown that
         # multiple handlers increases overall pipeline throughput
@@ -141,76 +163,72 @@ class Pipeline(object):
             reply_handler_thread.start()
             self.threads.append(reply_handler_thread)
 
-
     def execute(self, value):
-        """
-        """
+        """"""
         if not self.initialized:
-            raise DependenciesNotMetError("Pipeline's init method must be called before execute")
-        
+            raise DependenciesNotMetError(
+                "Pipeline's init method must be called before execute"
+            )
         # if the value isn't iterable, put it in a list
-        if not type(value).__name__ in ['generator', 'list']:
+        if not type(value).__name__ in ["generator", "list"]:
             value = [value]
-
         # Create the message envelopes and execute the pipeline from
-        # the entry nodes 
+        # the entry nodes
         for v in value:
             message = create_new_message(v, sample_rate=self.sample_rate)
             for entry in self.entry_nodes:
                 get_queue(entry).put(message, False)
-
 
     def close(self):
 
         if self.initialized:
             # call all the closes
             for operation in self.all_operations:
-                operation_function = self.graph.nodes()[operation].get('function', NullOperation())
-                if hasattr(operation_function, 'close'):
+                operation_function = self.graph.nodes()[operation].get(
+                    "function", NullOperation()
+                )
+                if hasattr(operation_function, "close"):
                     operation_function.close()
-
 
     def running(self):
         return not queues_empty()
 
-
     def read_sensors(self):
         readings = []
         for operation in self.all_operations:
-            readings.append(self.graph.nodes()[operation].get('function').read_sensor())
+            readings.append(self.graph.nodes()[operation].get("function").read_sensor())
         return readings
 
-
     # adapted from https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
-    def tree(self, node, prefix=''):
+    def tree(self, node, prefix=""):
 
-        space =  '    '
-        branch = ' │  '
-        tee =    ' ├─ '
-        last =   ' └─ '
+        space = "    "
+        branch = " │  "
+        tee = " ├─ "
+        last = " └─ "
 
-        contents = [ node[1] for node in self.graph.out_edges(node, default=[]) ]
+        contents = [node[1] for node in self.graph.out_edges(node, default=[])]
         # contents each get pointers that are ├── with a final └── :
         pointers = [tee] * (len(contents) - 1) + [last]
         for pointer, child_node in zip(pointers, contents):
             yield prefix + pointer + child_node
-            if len(self.graph.out_edges(node, default=[])) > 0: # extend the prefix and recurse:
-                extension = branch if pointer == tee else space 
+            if (
+                len(self.graph.out_edges(node, default=[])) > 0
+            ):  # extend the prefix and recurse:
+                extension = branch if pointer == tee else space
                 # i.e. space because last, └── , above so no more |
-                yield from self.tree(child_node, prefix=prefix+extension)
-
+                yield from self.tree(child_node, prefix=prefix + extension)
 
     def draw(self):
-        print('Pipeline Entry')
+        print("Pipeline Entry")
         for entry in self.entry_nodes:
-            print(' └─ {}'.format(entry))
-            t = self.tree(entry, '    ')
-            print('\n'.join(t))
-
+            print(" └─ {}".format(entry))
+            t = self.tree(entry, "    ")
+            print("\n".join(t))
 
     def flow(self):
-        result = { }
+        result = {}
         for operation in self.all_operations:
-            contents = [ node[1] for node in self.graph.out_edges(operation, default=[]) ]
+            contents = [node[1] for node in self.graph.out_edges(operation, default=[])]
             result[operation] = contents
         return result
