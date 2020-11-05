7f2447906527b0b596dbfbb59df0ed41aaa44db0
Justin Joyce <justin.joyce@joocer.com>
2020-11-05 22:50:55 +0000

    fixed tracer init

---
+++ source/cronicl/providers/google/__init__.py
@@ -0,0 +1,1 @@
+from .stackdrivertracer import StackDriverTracer
--- source/cronicl/tracers/__init__.py
+++ source/cronicl/tracers/__init__.py
@@ -1,3 +1,2 @@
 from .filetracer import FileTracer
 from .nulltracer import NullTracer
-from .stackdrivertracer import StackDriverTracer
